30350b5e16eaab65975e35a3ef9d2fe1e8db2a28
raphaazevedo <azevedo.almeida.ph@gmail.com>
2024-02-08 22:38:00 -0300

    first commit

---
+++ src/main/java/br/com/rapha/ApiContatos2Application.java
@@ -0,0 +1,13 @@
+package br.com.rapha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContatos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContatos2Application.class, args);
+	}
+
+}
